ee2bc7cf22fb913636ba54311c6de321f2d958e0
Davi Rodrigues <30713947+daviirodrig@users.noreply.github.com>
2022-06-13 06:39:56 +0000

    changed dates route to prisma

--- src/modules/dates/dates.module.ts
+++ src/modules/dates/dates.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntryRepository } from 'src/shared/repositories/entry.repository';
+import { PrismaService } from '../database/prisma.service';
 import { DatesController } from './dates.controller';
 import { DatesService } from './dates.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EntryRepository])],
   controllers: [DatesController],
-  providers: [DatesService],
+  providers: [DatesService, PrismaService],
 })
 export class DatesModule {}
--- src/modules/dates/dates.service.ts
+++ src/modules/dates/dates.service.ts
@@ -10,6 +10,9 @@ export class DatesService {
       orderBy: {
         date: 'asc',
       },
+      select: {
+        date: true,
+      },
     });
 
     const res = {
