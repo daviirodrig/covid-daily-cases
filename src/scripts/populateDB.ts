3a8102200a556121ef963894b6c429e3a92a4bab
Davi Rodrigues <30713947+daviirodrig@users.noreply.github.com>
2022-06-13 06:40:09 +0000

    change populateDB script to prisma

--- src/scripts/populateDB.ts
+++ src/scripts/populateDB.ts
@@ -1,19 +1,20 @@
-import { Entry } from '../shared/entities/entry.entity';
-import { createConnection } from 'typeorm';
+import { PrismaClient } from '@prisma/client';
 import * as csv from 'csvtojson';
 import fetch from 'node-fetch';
 
 async function populateDB(): Promise<void> {
-  const connection = await createConnection();
+  const prisma = new PrismaClient();
 
   const csvUrl = 'https://challenges.coode.sh/covid/data/covid-variants.csv';
   const csvFetch = await fetch(csvUrl);
   const csvFile = await csvFetch.text();
 
+  const entries = [];
+
   const jsonArray = await csv().fromString(csvFile);
 
   for (const i of jsonArray) {
-    const entry = new Entry();
+    const entry: any = {};
 
     entry.location = i.location;
     entry.date = new Date(i.date);
@@ -22,11 +23,21 @@ async function populateDB(): Promise<void> {
     entry.perc_sequences = Number(i.perc_sequences);
     entry.num_sequences_total = Number(i.num_sequences_total);
 
-    await entry.save();
-
+    //prisma.entry.createMany([entry]).then(() => {
+    //  console.log('Entry created');
+    //}
+    entries.push(entry);
     console.count('Entry saved');
   }
 
-  await connection.close();
+  prisma.entry
+    .createMany({
+      data: entries,
+    })
+    .then(() => {
+      console.log('All Entries created');
+    });
+
+  prisma.$disconnect();
 }
 populateDB();
